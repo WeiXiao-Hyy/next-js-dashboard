1c8f09f3d0a1b9db9b463e749cdb2189549db66f
hujiale <102018984+WeiXiao-Hyy@users.noreply.github.com>
2024-06-10 00:08:10 +0800

    update search table

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,37 @@
-export default function Page() {
+import {lusitana} from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import {CreateInvoice} from "@/app/ui/invoices/buttons";
+import {Suspense} from "react";
+import {InvoicesTableSkeleton} from "@/app/ui/skeletons";
+import InvoicesTable from "@/app/ui/invoices/table";
+
+
+export default async function Page({
+                                     searchParams,
+                                   }: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  }
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
   return (
-    <div>Invoices Page</div>
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices"/>
+        <CreateInvoice/>
+      </div>
+      <div className="mt-5 flex w-full justify-center">
+        <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton/>}>
+          <InvoicesTable query={query} currentPage={currentPage}/>
+        </Suspense>
+      </div>
+    </div>
   )
 }
--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -14,7 +14,7 @@ export default async function InvoicesTable({
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
-    <div className="mt-6 flow-root">
+    <div className="mt-6 flow-root w-full">
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
