1190f8cf4cbb5dad7f25366aecd111864d66808e
hujiale <102018984+WeiXiao-Hyy@users.noreply.github.com>
2024-06-11 08:53:04 +0800

    add: query

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,6 +4,8 @@ import {CreateInvoice} from "@/app/ui/invoices/buttons";
 import {Suspense} from "react";
 import {InvoicesTableSkeleton} from "@/app/ui/skeletons";
 import InvoicesTable from "@/app/ui/invoices/table";
+import {fetchInvoicesPages} from "@/app/lib/data";
+import Pagination from "@/app/ui/invoices/pagination";
 
 
 export default async function Page({
@@ -17,6 +19,8 @@ export default async function Page({
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
 
+  const totalPages = await fetchInvoicesPages(query);
+
   return (
     <div className="w-full">
       <div className="flex w-full items-center justify-between">
@@ -27,10 +31,15 @@ export default async function Page({
         <Search placeholder="Search invoices"/>
         <CreateInvoice/>
       </div>
+
       <div className="mt-5 flex w-full justify-center">
         <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton/>}>
           <InvoicesTable query={query} currentPage={currentPage}/>
         </Suspense>
+      </div>
+
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages}></Pagination>
       </div>
     </div>
   )
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import {MagnifyingGlassIcon} from '@heroicons/react/24/outline';
 import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import {useDebouncedCallback} from "use-debounce";
 
 export default function Search({placeholder}: { placeholder: string }) {
 
@@ -9,9 +10,10 @@ export default function Search({placeholder}: { placeholder: string }) {
   const pathname = usePathname();
   const {replace} = useRouter();
 
-  function handlerSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
 
+  const handlerSearch = useDebouncedCallback((term) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -19,8 +21,7 @@ export default function Search({placeholder}: { placeholder: string }) {
     }
 
     replace(`${pathname}?${params.toString()}`);
-  }
-
+  });
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
