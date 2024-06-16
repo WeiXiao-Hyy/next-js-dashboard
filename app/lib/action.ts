bd388730e124cc0d13b40558b49e8fbe4ec9d46e
hujiale <102018984+WeiXiao-Hyy@users.noreply.github.com>
2024-06-16 10:32:56 +0800

    fix: ts check

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -59,7 +59,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
   } catch (error) {
     // If a database error occurs, return a more specific error.
     return {
-      message: 'Database Error: Failed to Create Invoice.',
+      errors: {},
+      message: 'Database Error: Failed to Create Invoice.'
     };
   }
 
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -74,6 +74,14 @@ export default function Form({customers}: { customers: CustomerField[] }) {
                 className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
             </div>
           </div>
+          <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount &&
+              state.errors.amount.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
+          </div>
         </div>
 
         {/* Invoice Status */}
@@ -114,6 +122,14 @@ export default function Form({customers}: { customers: CustomerField[] }) {
                 </label>
               </div>
             </div>
+          </div>
+          <div id="amount-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.status &&
+              state.errors.status.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </fieldset>
       </div>
